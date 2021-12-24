c5774853fe72c1f404cd759bab9b70b914bfe5b0
LiamCrsd <43929554+LiamCrsd@users.noreply.github.com>
2021-12-24 11:52:31 +0100

    Add files via upload

---
+++ Dijkstra.py
@@ -0,0 +1,32 @@
+import math
+from collections import defaultdict
+from queue import PriorityQueue
+
+def dijkstra(graphe:dict, start_vertex:tuple) -> (dict, dict):
+    graphe = {**graphe}
+    D = {e: math.inf for e in graphe.keys()}
+    V = {}
+
+    D[start_vertex] = 0
+    V[start_vertex] = None
+
+    pq = PriorityQueue()
+    pq.put((0, start_vertex))
+
+    while not pq.empty():
+        dist, current_vertex = pq.get()
+        try:
+            liste = graphe[current_vertex].items()
+            del graphe[current_vertex]
+            for neighbor, distance in liste:
+
+                old_cost : int = D[neighbor]
+                new_cost : int = D[current_vertex] + distance
+
+                if new_cost < old_cost:
+                    pq.put((new_cost, neighbor))
+                    V[neighbor] : tuple = current_vertex
+                    D[neighbor] : int = new_cost
+        except:
+            pass
+    return D, V
